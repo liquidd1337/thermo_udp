a248f4701c06242be1b7a5318f488ccf80b91510
liquidd1337 <70625134+liquidd1337@users.noreply.github.com>
2023-12-17 19:45:29 +0700

    Delete target directory

--- target/debug/deps/udp_recv-da113cd9903981d3.d
+++
@@ -1,5 +0,0 @@
-C:\Users\mikle\Desktop\smhouse\thermo_udp\target\debug\deps\udp_recv-da113cd9903981d3.rmeta: src\bin\udp_recv.rs
-
-C:\Users\mikle\Desktop\smhouse\thermo_udp\target\debug\deps\udp_recv-da113cd9903981d3.d: src\bin\udp_recv.rs
-
-src\bin\udp_recv.rs:
--- target/debug/deps/udp_sender-0ce4c2286d2785c1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\mikle\Desktop\smhouse\thermo_udp\target\debug\deps\udp_sender-0ce4c2286d2785c1.rmeta: src\bin\udp_sender.rs
-
-C:\Users\mikle\Desktop\smhouse\thermo_udp\target\debug\deps\udp_sender-0ce4c2286d2785c1.d: src\bin\udp_sender.rs
-
-src\bin\udp_sender.rs:
